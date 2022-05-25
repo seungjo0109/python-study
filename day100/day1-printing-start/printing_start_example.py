6496718c606af1ad2c2534b6d25b3c360992e580
seungjo0109 <seungjo0109@gmail.com>
2022-05-25 22:26:10 +0900

    Add day100 - day1(printing test)

---
+++ day100/day1-printing-start/printing_start_example.py
@@ -0,0 +1,21 @@
+# example 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
+
+# example 2(debugging)
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.\n")
+
+# example 3
+print(len(input("what is your name?")))
+
+# example 4
+a = input("a: ")
+b = input("b: ")
+a, b = b, a
+print(f"a = {a}")
+print(f"b = {b}")
+
